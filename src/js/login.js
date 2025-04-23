75e993403e1c7bcbb118d316bacc28a6f2aec0eb
Balbino-teste <balbinojteste@gmail.com>
2025-04-22 22:36:06 -0300

    termino do formulário de login
    Nessa seção terminei de criar o formulário de login com uma adição de json e o termino do script para direcionamento de página e salvamento de dados nos browse.

--- src/js/login.js
+++ src/js/login.js
@@ -1,10 +1,21 @@
-const input = document.querySelector('.login-input')
-const button = document.querySelector('login-button')
+const input = document.querySelector('.login-input');
+const button = document.querySelector('.login-button');
+const form = document.querySelector('.login-form');
 
 const validateInput = ({target})=> {
-   if (target.value.length > 3)
-    button.removeAttribute('disabled')
-    
+   if (target.value.length > 3) {
+        button.removeAttribute('disabled');
+    }else {
+        button.setAttribute('disabled', '');
+    }
 }
 
-input.addEventListener('input',validateInput)
+const handleSubmit = (event) =>{
+    event.preventDefault();
+    
+    localStorage.setItem('player', input.value);
+    window.location = 'pages/game.html';
+}
+
+input.addEventListener('input',validateInput);
+form.addEventListener('submit', handleSubmit);
